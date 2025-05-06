ef69bde79a306ad2af0f6119a0715487bbc4493b
yamenarhim10 <yamenarhim@gmail.com>
2025-05-06 12:07:09 +0300

    Added Api

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #3rd party
+    'rest_framework',
     #Local
     "todos.apps.TodosConfig",
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#added 3rd party apps settings
+REST_FRAMEWOKR = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
